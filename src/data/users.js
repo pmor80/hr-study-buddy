a07a56947452c703ca87f83731aa18267585be10
pm1v21 <p.morawski@soton.ac.uk>
2025-03-05 11:19:12 +0000

    Add User List Componenet

---
+++ src/data/users.js
@@ -0,0 +1,27 @@
+export const users = [
+  {
+    name: 'Pawel Morawski',
+    attendance: '39%',
+    average: '2.3',
+  },
+  {
+    name: 'Adam Romanski',
+    attendance: '59%',
+    average: '4.3',
+  },
+  {
+    name: 'Albert Zeplinski',
+    attendance: '19%',
+    average: '1.3',
+  },
+  {
+    name: 'Marian Kowalski',
+    attendance: '29%',
+    average: '1.2',
+  },
+  {
+    name: 'Krzysztof Dabrowski',
+    attendance: '79%',
+    average: '4.0',
+  },
+];
